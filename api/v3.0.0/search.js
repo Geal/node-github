61ef5974618737e303552b75ebddfeb4ed904959
Geoffroy Couprie <geo.couprie@gmail.com>
2015-03-09 18:06:29 +0100

    implement code search

--- api/v3.0.0/search.js
+++ api/v3.0.0/search.js
@@ -63,6 +63,36 @@ var search = module.exports = {
         });
     };
 
+    this.code = function(msg, block, callback) {
+        var self = this;
+        this.client.httpSend(msg, block, function(err, res) {
+            if (err)
+                return self.sendError(err, null, msg, callback);
+
+            var ret;
+            try {
+                ret = res.data && JSON.parse(res.data);
+            }
+            catch (ex) {
+                if (callback)
+                    callback(new error.InternalServerError(ex.message), res);
+                return;
+            }
+
+            if (!ret)
+                ret = {};
+            if (!ret.meta)
+                ret.meta = {};
+            ["x-ratelimit-limit", "x-ratelimit-remaining", "x-ratelimit-reset", "x-oauth-scopes", "link", "location", "last-modified", "etag", "status"].forEach(function(header) {
+                if (res.headers[header])
+                    ret.meta[header] = res.headers[header];
+            });
+
+            if (callback)
+                callback(null, ret);
+        });
+    };
+
     /** section: github
      *  search#repos(msg, callback) -> null
      *      - msg (Object): Object that contains the parameters and their values to be sent to the server.
